cf399aa46918f09d2737239828fe2ba91f3b7fa2
杨涛 <1347781778@qq.com>
2020-02-15 15:06:41 +0800

    homepage

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,13 +1,88 @@
 import React, {Component} from 'react'
 import {
-    HomepageWrapper
+    HomepageWrapper,
+    HomepageContainer,
+    HomepageHeader,
+    HomepageContent
 }from './style'
 
 
 class Homepage extends Component {
     render(){
         return(
-            <HomepageWrapper></HomepageWrapper>
+            <HomepageWrapper>
+                <HomepageContainer>
+                    <HomepageHeader>
+                        <div className="content-container">
+                            <img className="avatar" src="https://www.yangicheng.cn/static/image/avatar.jpg" alt=""/>
+                            <div className="font-container">
+                                <p className="font-name">YooY</p>
+                                <p className="font-introduction">Never say die.</p>
+                            </div>
+                        </div>
+                        <div className="like-container">
+                            <i className="iconfont like-icon">&#xea66;</i>
+                            <p className="like-number">99</p>
+                        </div>
+                    </HomepageHeader>
+                    <HomepageContent>
+                        <div className="introduction">
+                            <div className="introduction-title">About Me:</div>
+                            <div className="introduction-container">
+                                <div className="introduction-name">
+                                    <div className="iconfont name-icon">&#xe9d9;</div>
+                                    <div className="name">you can call me Towel</div>
+                                </div>
+                                <div className="introduction-hobby">
+                                    <div className="iconfont hobby-icon">&#xe9d9;</div>
+                                    <div className="hobby">code game movie music</div>
+                                </div>
+                                <div className="introduction-aboutWeb">
+                                    <div className="iconfont web-icon">&#xe9d9;</div>
+                                    <div className="web">xxxxxx</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="works">
+                            <div className="github-container">
+                                <div className="github-title">My Github:</div>
+                                <div className="github-content">
+                                    <img src="https://www.yangicheng.cn/static/image/git.png" alt=""/>
+                                </div>
+                            </div>
+                            <div className="music-container">
+                                <div className="music-title">My music:</div>
+                                <div className="music-content">
+                                    <img src="https://www.yangicheng.cn/static/image/git.png" alt=""/>
+                                </div>
+                            </div>                        
+                        </div>
+                        <div className="recommend">
+                            
+                            <div className="recommend-course">
+                                <div className="course-container">
+                                    <img className="course-img"src="https://www.yangicheng.cn/static/image/7yue_python.jpg" alt=""/>
+                                    <div className="comment">7yue python</div>
+                                </div>
+                                <div className="course-container">
+                                    <img className="course-img"src="https://www.yangicheng.cn/static/image/7yue_python.jpg" alt=""/>
+                                    <div className="comment">bobo AI</div>
+                                </div>
+                                <div className="course-container">
+                                    <img className="course-img"src="https://www.yangicheng.cn/static/image/7yue_python.jpg" alt=""/>
+                                    <div className="comment">shuangyue</div>
+                                </div>
+                                <div className="course-container">
+                                    <img className="course-img"src="https://www.yangicheng.cn/static/image/7yue_python.jpg" alt=""/>
+                                    <div className="comment">dell react</div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </HomepageContent>
+                </HomepageContainer>
+
+            </HomepageWrapper>
         )
     }
 }
--- src/pages/homepage/style.js
+++ src/pages/homepage/style.js
@@ -1,8 +1,188 @@
 import styled from 'styled-components'
 
-export const HomepageWrapper = styled.div `
-    width:100px;
-    height:100px;
-    background: red;
+export const HomepageWrapper = styled.div`
+    background: #F2EEDF;
+`
+export const HomepageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 0 12%;
+`
+export const HomepageHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top:5%;
+    .content-container{
+        display:flex;
+        flex-direction: row;
+        jusitfy-content: center;
+        align-items: center;
+        .avatar{
+            height:100px;
+            width:100px;
+            margin:10px 20px 10px 10px;
+            clip-path: circle(50% at 50% 50%);
+        }
+        .font-container{
+            display:flex;
+            flex-direction: column;
+            jusitfy-content: center;
+            align-items: center;
+            line-height:40px;
+            .font-name{
+                width:100%;
+                font-size:30px;
+                font-weight:700;
+                margin-bottom:10px;
+                font-family: "Arial";
+            }
+            .font-introduction{
+                font-size:20px;
+                width:100%;
+                font-family: "Arial";
+            }
+        }
+    }
+    .like-container{
+        display:flex;
+        flex-direction: row;
+        line-height:40px;
+        cursor: pointer;
+        position:relative;
+        top:-30px;
+        .like-icon{
+            font-size:30px;
+            margin-right:10px;
+        }
+        .like-number{
+            font-size:15px;
+            font-family: "Times new Roman";
+        }
+    }
+`
 
+export const HomepageContent = styled.div`
+    dispaly: flex;
+    flex-direction: column;
+    width:100%;
+    margin-top: 20px;
+    .introduction{
+        display:flex;
+        flex-direction: column;
+        box-sizing: border-box;
+        padding: 10px 10px;
+        width:100%;
+        border: 1px solid black;
+        .introduction-title{
+            line-height:40px;
+            font-size: 30px;
+            font-weight: 400px;
+        }
+        .introduction-container{
+            display:flex;
+            flex-direction: column;
+            .introduction-name{
+                display:flex;
+                flex-direction: row;
+                line-height: 40px;
+                .name-icon{
+                    margin-right: 20px;
+                }
+                .name{
+                    font-size: 20px;
+                }
+            }
+            .introduction-hobby{
+                display:flex;
+                flex-direction: row;
+                line-height: 40px;
+                .hobby-icon{
+                    margin-right: 20px;
+                }
+                .hobby{
+                    font-size: 20px;
+                }
+            }
+            .introduction-aboutWeb{
+                display:flex;
+                flex-direction: row;
+                line-height: 40px;
+                .web-icon{
+                    margin-right: 20px;
+                }
+                .web{
+                    font-size: 20px;
+                }
+            }
+        }
+    }
+
+    .github-container{
+        display:flex;
+        margin-top: 20px;
+        flex-direction: column;
+        box-sizing: border-box;
+        padding: 10px 10px;
+        border:1px solid black;
+        .github-title{
+            line-height:40px;
+            font-size: 30px;
+            font-weight: 400px;
+        }
+        .github-content{
+            img{
+                height:400px;
+                width: 400px;
+            }
+        }
+    }
+    .music-container{
+        display:flex;
+        margin-top: 20px;
+        flex-direction: column;
+        box-sizing: border-box;
+        padding: 10px 10px;
+        border:1px solid black;
+        .music-title{
+            line-height:40px;
+            font-size: 30px;
+            font-weight: 400px;
+        }
+        .music-content{
+            display:flex;
+            img{
+                height:400px;
+                width: 400px;
+            }
+        }
+    }
+    .recommend{
+        display:flex;
+        border:1px solid black;
+        margin-top: 10px;
+        box-sizing: border-box;
+        padding: 10px 20px;
+        width:100%;
+        .recommend-course{
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr 1fr;
+            grid-template-rows   : auto;
+            gap                  : 20px;
+            width:100%;
+            .course-container{
+                display:flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                .course-img{
+                    height: 100px;
+                    width: 180px;
+                    border-radius: 8px;
+                }
+            }
+        }
+    }
+    
 `
