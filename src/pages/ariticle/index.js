d259c1df91754ef2d0b42ac290894a97b5007fd8
杨涛 <1347781778@qq.com>
2020-02-16 09:45:53 +0800

    article

--- src/pages/ariticle/index.js
+++ src/pages/ariticle/index.js
@@ -1,13 +1,105 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import {
-    ArticleWrapper
-}from './style'
-
+    ArticleWrapper,
+    ArticleContainer,
+    ArticleHeader,
+    ArticleContent,
+    ArticleLeft,
+    ArticleRight
+} from './style'
+import Swiper from 'swiper/js/swiper';
+import 'swiper/css/swiper.css'
 
 class Article extends Component {
-    render(){
-        return(
-            <ArticleWrapper></ArticleWrapper>
+    componentDidMount() {
+        new Swiper('.swiper-container', {
+            loop: true, // 循环模式选项
+            // autoplay: true,
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
+    render() {
+        return (
+            <ArticleWrapper>
+                <ArticleContainer>
+                    <ArticleHeader>
+                        <div className="title"> مقالات </div>
+                    </ArticleHeader>
+                    <ArticleContent>
+                        <ArticleLeft>
+                            <div className="swiper-container">
+                                <div className="swiper-wrapper">
+                                    <div className="swiper-slide">
+                                        <img className="swiper-img" src="https://www.yangicheng.cn/static/image/cover.jpg" alt="" />
+                                    </div>
+                                    <div className="swiper-slide">
+                                        <img className="swiper-img" src="https://www.yangicheng.cn/static/image/cover.jpg" alt="" />
+                                    </div>
+                                    <div className="swiper-slide">
+                                        <img className="swiper-img" src="https://www.yangicheng.cn/static/image/cover.jpg" alt="" />
+                                    </div>
+                                </div>
+                                <div className="swiper-pagination"></div>
+                                <div className="swiper-button-prev"></div>
+                                <div className="swiper-button-next"></div>
+                            </div>
+
+                            <div className="article-container">
+                                <div className="article-item">
+                                    <img src="https://www.yangicheng.cn/static/image/cover.jpg" alt="" className="article-img"/>
+                                    <div className="article-content">
+                                        <div className="article-title">关于个人博客</div>
+                                        <div className="article-summary">
+                                            <div className="label">python</div>
+                                            <div className="date">2.15</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="article-item">
+                                    <img src="https://www.yangicheng.cn/static/image/cover.jpg" alt="" className="article-img"/>
+                                    <div className="article-content">
+                                        <div className="article-title">关于个人博客</div>
+                                        <div className="article-summary">
+                                            <div className="label">python</div>
+                                            <div className="date">2.15</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="article-item">
+                                    <img src="https://www.yangicheng.cn/static/image/cover.jpg" alt="" className="article-img"/>
+                                    <div className="article-content">
+                                        <div className="article-title">关于个人博客</div>
+                                        <div className="article-summary">
+                                            <div className="label">python</div>
+                                            <div className="date">2.15</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="article-item">
+                                    <img src="https://www.yangicheng.cn/static/image/cover.jpg" alt="" className="article-img"/>
+                                    <div className="article-content">
+                                        <div className="article-title">关于个人博客</div>
+                                        <div className="article-summary">
+                                            <div className="label">python</div>
+                                            <div className="date">2.15</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </ArticleLeft>
+                        <ArticleRight>
+                            <div>sideBar</div>
+                        </ArticleRight>
+                    </ArticleContent>
+                </ArticleContainer>
+
+            </ArticleWrapper>
         )
     }
 }
--- src/pages/ariticle/style.js
+++ src/pages/ariticle/style.js
@@ -1,8 +1,111 @@
 import styled from 'styled-components'
 
 export const ArticleWrapper = styled.div `
-    width:100px;
-    height:100px;
-    background: green;
-
+    background:#F2EEDF;
+`
+export const ArticleContainer = styled.div `
+    display: flex;
+    flex-direction: column;
+    margin: 0 12%;
+`
+export const ArticleHeader = styled.div `
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 100px;
+    .title{
+        font-family: "Lohit Devanagari", "ITF Devanagari";
+        font-size:30px;
+        font-weight: 400;
+    }
+`
+export const ArticleContent = styled.div `
+    display: flex;
+    flex-direction: row;
+    
+`
+export const ArticleLeft = styled.div `
+    display:flex;
+    width: 70%;
+    box-sizing: border-box;
+    padding:10px 10px;
+    flex-direction: column;
+    .swiper-container{
+        width:100%;
+        height:300px;
+        .swiper-img{
+            height:300px;
+            width:100%;
+            border-radius: 8px;
+        }
+    }
+    .article-container{
+        margin-top:20px;
+        background: #fff;
+        border-radius: 9px;
+        box-sizing: border-box;
+        padding:10px 10px;
+        box-shadow: 3px 3px 3px #dedede;
+        .article-item{
+            display:flex;
+            width:100%;
+            height:100px;
+            flex-direction: row;
+            align-items: center;
+            .article-img{
+                height: 80px;
+                width: 80px;
+                border-radius: 10px;
+                margin-right: 20px;
+            }
+            .article-content{
+                display:flex;
+                flex-direction: column;
+                width:550px;
+                height: 95px;
+                justify-content: center;
+                border-bottom: 1px solid #dedede;
+                .article-title{
+                    font-weight: 700;
+                    margin-bottom: 10px;
+                    font-size: 16px;
+                    color: #1c1f21;
+                    line-height: 30px;
+                    word-break: keep-all;
+                    white-space: nowrap;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    width:100%;
+                    cursor: pointer;
+                }
+                .article-summary{
+                    display: flex;
+                    flex-direction:row;
+                    justify-content: space-between;
+                    .label{
+                        padding: 4px 12px;
+                        font-size: 12px;
+                        color: #545c63;
+                        line-height: 16px;
+                        background: rgba(84,92,99,0.1);
+                        border-radius: 12px;
+                        cursor: pointer;
+                    }
+                    .date{
+                        font-size: 12px;
+                        color: #9199a1;
+                        line-height: 18px;
+                        padding-top: 3px;
+                    }
+                }
+            }
+        }
+    }
+`
+export const ArticleRight = styled.div `
+    display:flex;
+    width: 30%;
+    flex-direction: column;
+    
 `
